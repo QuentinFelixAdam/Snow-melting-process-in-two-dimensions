d65ead54b2c88d9c4a8274f4708e384a6eb8f089
QuentinFelixAdam <93124691+QuentinFelixAdam@users.noreply.github.com>
2022-06-16 14:51:40 +0200

    Add files via upload

--- sub_annex.f
+++ sub_annex.f
@@ -2,18 +2,102 @@ SUBROUTINE lire()
 USE m_global
 IMPLICIT NONE
 
+PRINT*,'-----------------------------------'
+PRINT*,'------SIMULATION HAS STARTED-------'
+PRINT*,'-----------------------------------'
+PRINT*,'-----------LOADING DATA------------'
+
 ! FILES FOR CASE
 
-nlinescase = 11*10000
+nlinescase = 0
+
+OPEN(36,FILE='Windspeedsynt.dat',STATUS='old')
+
+DO
+   READ(36,*,END=22)
+   nlinescase = nlinescase + 1
+END DO
+22 CLOSE(36)
 
 ALLOCATE(V_air(nlinescase))
 
+OPEN(36,FILE='Windspeedsynt.dat',STATUS='old')
+
+DO i = 1,nlinescase
+   READ(36,*) V_air(i)
+END DO
+
+CLOSE(36)
+
+PRINT*,'...'
+
+nlinescase = 0
+
+OPEN(36,FILE='Shortwavesynt.dat',STATUS='old')
+
+DO
+   READ(36,*,END=23)
+   nlinescase = nlinescase + 1
+END DO
+23 CLOSE(36)
+
 ALLOCATE(f_s(nlinescase))
+
+OPEN(36,FILE='Shortwavesynt.dat',STATUS='old')
+
+DO i = 1,nlinescase
+   READ(36,*) f_s(i)
+END DO
+
+CLOSE(36)
+
+
+PRINT*,'...'
+
+nlinescase = 0
+
+OPEN(36,FILE='Airtempsynt.dat',STATUS='old')
+
+DO
+   READ(36,*,END=24)
+   nlinescase = nlinescase + 1
+END DO
+24 CLOSE(36)
 
 ALLOCATE(T_air(nlinescase))
 
+OPEN(36,FILE='Airtempsynt.dat',STATUS='old')
+
+DO i = 1,nlinescase
+   READ(36,*) T_air(i)
+END DO
+
+CLOSE(36)
+
+PRINT*,'...'
+
+nlinescase = 0
+
+OPEN(36,FILE='Longwavesynt.dat',STATUS='old')
+
+DO
+   READ(36,*,END=25)
+   nlinescase = nlinescase + 1
+END DO
+25 CLOSE(36)
+
 ALLOCATE(fD(nlinescase))
 
+OPEN(36,FILE='Longwavesynt.dat',STATUS='old')
+
+DO i = 1,nlinescase
+   READ(36,*) fD(i)
+END DO
+
+CLOSE(36)
+
+PRINT*,'...'
+PRINT*,'-----------DATA LOADED-------------'
 
 END SUBROUTINE lire
 
